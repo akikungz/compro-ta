cb204f12a0dd85e5ce5867b270f2daa8470b843f
Thitipong Tapianthong <akikungz@proton.me>
2024-07-08 15:05:55 +0700

    chore: Add arithmetic and operator precedence tests

---
+++ lecture-02/test_arithmetic.py
@@ -0,0 +1,16 @@
+x = 15
+y = 4
+
+print(f"x + y = {x + y}")
+
+print(f"x - y = {x - y}")
+
+print(f"x * y = {x * y}")
+
+print(f"x / y = {x / y}")
+
+print(f"x % y = {x % y}")
+
+print(f"x // y = {x // y}")
+
+print(f"x ** y = {x ** y}")
